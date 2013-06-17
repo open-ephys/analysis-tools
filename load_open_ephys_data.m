aa61998bfb7cdffb748509bcd9309654a17b8472
jsiegle <jsiegle@mit.edu>
2013-06-17 12:05:51 -0400

    Add code for loading .spikes files

--- load_open_ephys_data.m
+++ load_open_ephys_data.m
@@ -1,4 +1,4 @@
-function [data, timestamps, info] = load_open_ephys_data(filename, use_updated_format)
+function [data, timestamps, info] = load_open_ephys_data(filename)
 
 %
 % [data, timestamps, info] = load_open_ephys_data(filename, use_updated_format)
@@ -18,7 +18,8 @@ function [data, timestamps, info] = load_open_ephys_data(filename, use_updated_f
 %
 %   Outputs:
 %
-%     data: either continuous samples or event channels
+%     data: either an array continuous samples, a matrix of spike waveforms,
+%           or an array of event channels
 %
 %     timestamps: sample time in seconds
 %
@@ -39,8 +40,6 @@ function [data, timestamps, info] = load_open_ephys_data(filename, use_updated_f
 %     ------------------------------------------------------------------
 % 
 %     Copyright (C) 2013 Open Ephys
-%
-%     Last updated by jsiegle on January 26, 2013
 % 
 %     ------------------------------------------------------------------
 % 
@@ -58,16 +57,15 @@ function [data, timestamps, info] = load_open_ephys_data(filename, use_updated_f
 %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 % 
 
-
-if nargin < 2
-    use_updated_format = 1; % include sample number in event data and record marker in continuous data
-end
-
-
 filetype = filename(max(strfind(filename,'.'))+1:end); % parse filetype
 
 fid = fopen(filename);
 filesize = getfilesize(fid);
+
+% constants for pre-allocating matrices:
+MAX_NUMBER_OF_SPIKES = 1e6; 
+MAX_NUMBER_OF_CONTINUOUS_SAMPLES = 10e6;
+MAX_NUMBER_OF_EVENTS = 1e5;
 
 %-----------------------------------------------------------------------
 %------------------------- EVENT DATA ----------------------------------
@@ -83,22 +81,35 @@ if strcmp(filetype, 'events')
     eval(char(hdr'));
     info.header = header;
     
+    % pre-allocate space for event data
+    data = zeros(MAX_NUMBER_OF_EVENTS, 1);
+    timestamps = zeros(MAX_NUMBER_OF_EVENTS, 1);
+    info.sampleNum = zeros(MAX_NUMBER_OF_EVENTS, 1);
+    info.nodeId = zeros(MAX_NUMBER_OF_EVENTS, 1);
+    info.eventType = zeros(MAX_NUMBER_OF_EVENTS, 1);
+    info.eventId = zeros(MAX_NUMBER_OF_EVENTS, 1);
+    
     while ftell(fid) + 22 < filesize % at least one record remains
         
         index = index + 1;
         
         timestamps(index) = fread(fid, 1, 'uint64', 0, 'l');
         
-        if (use_updated_format)
-            info.sampleNum(index) = fread(fid, 1, 'int16'); % implemented after 11/16/12
-        end
-        
+        info.sampleNum(index) = fread(fid, 1, 'int16'); % implemented after 11/16/12
         info.eventType(index) = fread(fid, 1, 'uint8');
         info.nodeId(index) = fread(fid, 1, 'uint8');
         info.eventId(index) = fread(fid, 1, 'uint8');
         data(index) = fread(fid, 1, 'uint8'); % save event channel as 'data' (maybe not the best thing to do)
         
     end
+    
+    % crop the arrays to the correct size
+    data(index+1:end) = [ ];
+    timestamps(index+1:end) = [ ];
+    info.sampleNum(index+1:end) = [ ];
+    info.nodeId(index+1:end) = [ ];
+    info.eventType(index+1:end) = [ ];
+    info.eventId(index+1:end) = [ ];    
     
 %-----------------------------------------------------------------------
 %---------------------- CONTINUOUS DATA --------------------------------
@@ -110,7 +121,8 @@ elseif strcmp(filetype, 'continuous')
     
     index = 0;
     
-    max_samples = 10e6; % pre-allocate 10 million samples for data
+    % pre-allocate samples for data
+    max_samples = MAX_NUMBER_OF_CONTINUOUS_SAMPLES;
     
     hdr = fread(fid, 1024, 'char*1');
     eval(char(hdr'));
@@ -147,7 +159,8 @@ elseif strcmp(filetype, 'continuous')
         
     end
     
-    data = data(1:current_sample); % get rid of extra zeros
+    % crop data to the correct size
+    data = data(1:current_sample); 
     
     timestamps = nan(size(data));
     
@@ -163,16 +176,87 @@ elseif strcmp(filetype, 'continuous')
     end
     
     % NOTE: the timestamps for the last record will not be interpolated
+   
+    
+%-----------------------------------------------------------------------
+%--------------------------- SPIKE DATA --------------------------------
+%-----------------------------------------------------------------------
+    
+elseif strcmp(filetype, 'spikes')
+        
+    disp(['Loading spikes file...']);
+    
+    index = 0;
+    
+    hdr = fread(fid, 1024, 'char*1');
+    eval(char(hdr'));
+    info.header = header;
+    
+    % data = 0;
+    % timestamps = 0;
+    
+    current_spike = 0;
+    
+    while ftell(fid) + 512 < filesize % at least one record remains
+     
+        current_spike = current_spike + 1;
+     
+        idx = 0;
+        
+        event_type = fread(fid, 1, 'uint8'); % always equal to 4; ignore
+        
+        idx = idx + 1;
+        
+        timestamps(current_spike) = fread(fid, 1, 'uint64', 0, 'l');
+        
+        idx = idx + 8;
+        
+        info.source(current_spike) = fread(fid, 1, 'uint16', 0, 'l');
+        
+        idx = idx + 2;
+        
+        num_channels = fread(fid, 1, 'uint16', 0, 'l');
+        num_samples = fread(fid, 1, 'uint16', 0, 'l');
+        
+        idx = idx + 4;
+        
+        if num_samples < 1 || num_samples > 10000
+            disp(['Loading failed at block number ' int2str(current_spike) '. Found ' ...
+                  int2str(num_samples) ' samples.'])
+              break;
+        end
+        
+        waveforms = fread(fid, num_channels*num_samples, 'uint16', 0, 'l');
+        
+        idx = idx + num_channels*num_samples*2;
+        
+        wv = reshape(waveforms, num_samples, num_channels);
+        
+        channel_gains = fread(fid, num_channels, 'uint16', 0, 'l');
+        
+        gain = double(repmat(channel_gains', num_samples, 1))/1000;
+        
+        channel_thresholds = fread(fid, num_channels, 'uint16', 0, 'l');
+        
+        idx = idx + num_channels*2*2;
+        
+        data(current_spike, :, :) = double(wv-32768)./gain;
+        
+        junk = fread(fid, 512-idx, 'uint8', 0, 'l');
+        
+    end
     
 else
     
-    error('Filetype not recognized.');
+    error('File extension not recognized. Please use a ''.continuous'', ''.spikes'', or ''.events'' file.');
     
 end
 
-fclose(fid);
+fclose(fid); % close the file
 
 end
+
+
 
 function filesize = getfilesize(fid)
 
